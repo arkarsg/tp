2e3e4e85346373a0c8a47a41acc6bd355ca10db9
arkarsg <alxarkar@gmail.com>
2023-02-20 19:41:14 +0800

    Adds JavaDoc for Remark

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Public constructor for Remark.
+     * @param remark on a Person
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
